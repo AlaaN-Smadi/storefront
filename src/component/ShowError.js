77cc2c9eac79af2c2b22c99e9a8ac76f2726bd87
AlaaN-Smadi <alaasmadi1010@gmail.com>
2021-07-06 11:37:12 +0300

    Solve some issue

--- src/component/ShowError.js
+++ src/component/ShowError.js
@@ -5,13 +5,14 @@ class ShowError extends React.Component {
     render() {
         return (
             <>
-            <div style={{ marginRight: "20%",textAlign:"center",background:"#4b4ca3", marginLeft: "20%",marginTop:"10px", border: "1px solid #000150", borderRadius: "30px" }}>
-                <div style={{ textAlign: "center", bordertRadius: "30px" }}>
-                    <img style={{ width: "100%", height: "500px", borderRadius: "30px" }} alt="Error" src={WrongMessage} />
+                <div style={{ position: "relative", marginRight: "20%", textAlign: "center", marginLeft: "20%", marginTop: "10px", border: "1px solid #000150", borderRadius: "30px" }}>
+                    <div style={{ textAlign: "center", bordertRadius: "30px" }}>
+                        <img style={{ width: "100%", height: "500px", borderRadius: "30px",zIndex:"1" }} alt="Error" src={WrongMessage} />
+
+                    </div>
+                    <div style={{ position: "absolute", width: "100%", height: "500px", borderRadius: "30px", backgroundColor: "#4b4ca3", opacity: "0.3", top: "0" }}>  </div>
+
                 </div>
-
-
-            </div>
             </>
         )
     }
