325c4ca3deb2fd6da5cbeebb6910b24806010691
AlaaN-Smadi <alaasmadi1010@gmail.com>
2021-09-28 20:36:14 +0300

    Solve lab 33

--- src/App.js
+++ src/App.js
@@ -2,14 +2,28 @@ import React from 'react';
 import ToDo from './component/ToDo/ToDo';
 import SettingProvider from './context';
 
+import Auth from "./component/context/auth";
+import LoginProvider from "./component/context/context";
+import Login from "./component/context/login";
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default class App extends React.Component {
   render() {
     return (
-      <SettingProvider>
-        <ToDo />
-      </SettingProvider>
+      <div>
+
+
+        <LoginProvider>
+          <SettingProvider>
+          
+            {/* user - editor - admin */}
+            
+              <ToDo />
+           
+          </SettingProvider>
+        </LoginProvider>
+      </div>
     );
   }
 }
